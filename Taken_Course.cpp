ac8e409234cc9da3f22d9edd1c9952c89869ff70
SAMPAT JANGID <43749236+jangidsampat@users.noreply.github.com>
2020-04-29 21:23:36 +0530

    Update Taken_Course.cpp

--- Taken_Course.cpp
+++ Taken_Course.cpp
@@ -1,20 +1,14 @@
 #include "Taken_Course.h"
 #include <iostream>
 
-Taken_Course::Taken_Course(string course,string result)
-{
+Taken_Course::Taken_Course(string course,string result) {
 	course_name = course;
 	grade =result;
 }
 
-void Taken_Course::print()
-{
-	cout<<course_name;
-	cout<<"  <------>   ";
-	cout<<grade;
-	cout<<endl;
+void Taken_Course::print() {
+	cout<<course_name<<"  ->>   "<<grade<<endl;
 }
 
-Taken_Course::~Taken_Course(void)
-{
+Taken_Course::~Taken_Course(void) {
 }
