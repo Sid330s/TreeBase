e159f4486c2449d9867b821ef6f3cf0d8b1b2b33
SAMPAT JANGID <43749236+jangidsampat@users.noreply.github.com>
2020-04-29 18:07:44 +0530

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,30 +1,24 @@
 #include "Student.h"
 #include <iostream>
 
-Student::Student(int roll,string student_name,vector<Taken_Course*> courses)
-{
+Student::Student(int roll,string student_name,vector<Taken_Course*> courses) {
 	roll_number = roll;
 	name = student_name;
 	courses_taken = courses;
 }
-int Student::get_roll_number()
-{
+
+int Student::get_roll_number() {
 	return roll_number;
 }
-void Student::print()
-{
+
+void Student::print() {
 	cout<<"Student Name : ";
 	cout<<name;
 	cout<<"  <---->  Roll Number : ";
 	cout<<roll_number<<endl;
-
 	cout<<"Course Name  <---->  Grade\n";
-	
-	for(Taken_Course* i : courses_taken) 
-		i->print();
-
+	for(Taken_Course* i : courses_taken) i->print();
 }
 
-Student::~Student(void)
-{
+Student::~Student(void) {
 }
