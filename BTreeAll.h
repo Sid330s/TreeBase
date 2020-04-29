0845b63ae1cc92fd84d67dadad1f89eab5fd16f5
SAMPAT JANGID <43749236+jangidsampat@users.noreply.github.com>
2020-04-29 17:37:12 +0530

    Update BTreeAll.h

--- BTreeAll.h
+++ BTreeAll.h
@@ -1,17 +1,16 @@
 #pragma once
 #include "BTreeNode.h"
 #include "Node.h"
-class BTree
-{
+
+class BTree {
 	BTreeNode *root; 
-	int t; // Minimum degree
-public:
+	int t;
 	
+public:	
 	BTree();
 	void set_order(int);
 	void traverse();
 	node* search(int k);
 	void insert(int k,node*);
-	// The main function that removes a new key in thie B-Tree
-    void remove(int k);
+	void remove(int k);
 };
