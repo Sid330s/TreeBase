60d45536395949da9ba2eda912e8984c149dd33d
SAMPAT JANGID <43749236+jangidsampat@users.noreply.github.com>
2020-04-29 17:35:34 +0530

    Update BTreeAll.cpp

--- BTreeAll.cpp
+++ BTreeAll.cpp
@@ -1,96 +1,63 @@
 #include "BTreeAll.h"
 #include <iostream>
-
 using namespace std;
 
-BTree::BTree()
-{ 
+BTree::BTree() { 
 	root = NULL;
 }
 
-
-void BTree::traverse()
-{ 
+void BTree::traverse() { 
 	if (root != NULL) root->traverse();
 }
 
-
-node* BTree::search(int k)
-{ 
+node* BTree::search(int k) { 
 	if ((root->search(k) == NULL) || (root == NULL))
 		return NULL;
-	else
-	{
+	else {
 		BTreeNode* rec_found = root->search(k);
-		for(int i=0;i<rec_found->n;i++)
-		{
+		for(int i=0;i<rec_found->n;i++) {
 			if (k==rec_found->keys[i].roll_no)
 				return rec_found->keys[i].record;
 		}
-	
 	}
 }
 
-
-void BTree::insert(int k,node* rec)
-{
-	if (root == NULL)
-	{
+void BTree::insert(int k,node* rec) {
+	if (root == NULL) {
 		root = new BTreeNode(t, true);
 		root->keys[0].roll_no = k; 
 		root->keys[0].record = rec;
 		root->n = 1; 
-	}
-	else 
-	{
-		if (root->n == 2*t-1)
-		{
+	} else {
+		if (root->n == 2*t-1) {
 			BTreeNode *s = new BTreeNode(t, false);
-
 			s->C[0] = root;
-
 			s->splitChild(0, root);
-
 			int i = 0;
-			if (s->keys[0].roll_no < k)
-				i++;
+			if (s->keys[0].roll_no < k) i++;
 			s->C[i]->insertNonFull(k,rec);
-
 			root = s;
-		}
-		else 
-			root->insertNonFull(k,rec);
+		} else  root->insertNonFull(k,rec);
 	}
 }
 
-
-void BTree::set_order(int _t)
-{
+void BTree::set_order(int _t) {
 	t= _t;
 }
-void BTree::remove(int k)
-{
-    if (!root)
-    {
-        cout << "The tree is empty\n";
-        return;
-    }
- 
-    // Call the remove function for root
-    root->remove(k);
- 
-    // If the root node has 0 keys, make its first child as the new root
-    //  if it has a child, otherwise set root as NULL
-    if (root->n==0)
-    {
-        BTreeNode *tmp = root;
-        if (root->leaf)
-            root = NULL;
-        else
-            root = root->C[0];
- 
-        // Free the old root
-        delete tmp;
-    }
-    return;
+
+void BTree::remove(int k) {
+	if (!root) {
+		cout << "The tree is empty\n";
+		return;
+	}
+
+	root->remove(k);
+
+	if (root->n==0) {
+		BTreeNode *tmp = root;
+		if (root->leaf) root = NULL;
+		else root = root->C[0];
+		delete tmp;
+	}
+	return;
 }
